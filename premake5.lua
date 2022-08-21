d307f70eff6ef63d9ed99051bf894f0e9dd8da0b
Alan-mag <alanmagelssen@gmail.com>
2022-08-21 15:09:54 -0700

    Add glfw and create window

--- premake5.lua
+++ premake5.lua
@@ -10,6 +10,11 @@ workspace "Amoeba"
     }
 
     outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+    -- Include directories relative to root folder (solution directory)
+    IncludeDir = {}
+    IncludeDir["GLFW"] = "Amoeba/vendor/GLFW/include"
+
+    include "Amoeba/vendor/GLFW"
 
 
 project "Amoeba"
@@ -32,7 +37,14 @@ project "Amoeba"
     includedirs
     {
         "%{prj.name}/src",
-        "%{prj.name}/vendor/spdlog/include"
+        "%{prj.name}/vendor/spdlog/include",
+		"%{IncludeDir.GLFW}"
+	}
+
+	links 
+	{ 
+		"GLFW",
+		"opengl32.lib"
     }
 
     filter "system:windows"
