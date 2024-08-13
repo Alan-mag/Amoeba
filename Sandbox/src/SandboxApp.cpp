446abcf918cb6e140f18e6710814b16f48a7d6a4
Alan Magelssen <alanmagelssen@gmail.com>
2024-08-12 17:23:42 -0700

    Timestep and OpenGLShader update

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,6 +1,10 @@
 #include <Amoeba.h>
 
 #include "imgui/imgui.h"
+#include "Platform/OpenGL/OpenGLShader.h"
+
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 class ExampleLayer : public Amoeba::Layer 
 {
@@ -33,10 +37,10 @@ public:
 		m_SquareVA.reset(Amoeba::VertexArray::Create());
 
 		float squareVertices[3 * 4] = {
-			-0.75f, -0.75f, 0.0f,
-			 0.75f, -0.75f, 0.0f,
-			 0.75f,  0.75f, 0.0f,
-			-0.75f,  0.75f, 0.0f
+			-0.5f, -0.5f, 0.0f,
+			 0.5f, -0.5f, 0.0f,
+			 0.5f,  0.5f, 0.0f,
+			-0.5f,  0.5f, 0.0f
 		};
 
 		std::shared_ptr<Amoeba::VertexBuffer> squareVB;
@@ -58,6 +62,7 @@ public:
 			layout(location = 1) in vec4 a_Color;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 			out vec4 v_Color;
@@ -66,7 +71,7 @@ public:
 			{
 				v_Position = a_Position;
 				v_Color = a_Color;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);	
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);	
 			}
 		)";
 
@@ -85,38 +90,41 @@ public:
 			}
 		)";
 
-		m_Shader.reset(new Amoeba::Shader(vertexSrc, fragmentSrc));
+		m_Shader.reset(Amoeba::Shader::Create(vertexSrc, fragmentSrc));
 
-		std::string blueShaderVertexSrc = R"(
+		std::string flatColorShaderVertexSrc = R"(
 			#version 330 core
 			
 			layout(location = 0) in vec3 a_Position;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 
 			void main()
 			{
 				v_Position = a_Position;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);	
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);	
 			}
 		)";
 
-		std::string blueShaderFragmentSrc = R"(
+		std::string flatColorShaderFragmentSrc = R"(
 			#version 330 core
 			
 			layout(location = 0) out vec4 color;
 
 			in vec3 v_Position;
+			
+			uniform vec3 u_Color;
 
 			void main()
 			{
-				color = vec4(0.2, 0.3, 0.8, 1.0);
+				color = vec4(u_Color, 1.0);
 			}
 		)";
 
-		m_BlueShader.reset(new Amoeba::Shader(blueShaderVertexSrc, blueShaderFragmentSrc));
+		m_FlatColorShader.reset(Amoeba::Shader::Create(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
 	}
 
 	void OnUpdate(Amoeba::Timestep ts) override
@@ -144,7 +152,21 @@ public:
 
 		Amoeba::Renderer::BeginScene(m_Camera);
 
-		Amoeba::Renderer::Submit(m_BlueShader, m_SquareVA);
+		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+
+		std::dynamic_pointer_cast<Amoeba::OpenGLShader>(m_FlatColorShader)->Bind();
+		std::dynamic_pointer_cast<Amoeba::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat3("u_Color", m_SquareColor);
+
+		for (int y = 0; y < 20; y++)
+		{
+			for (int x = 0; x < 20; x++)
+			{
+				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
+				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+				Amoeba::Renderer::Submit(m_FlatColorShader, m_SquareVA, transform);
+			}
+		}
+
 		Amoeba::Renderer::Submit(m_Shader, m_VertexArray);
 
 		Amoeba::Renderer::EndScene();
@@ -152,7 +174,9 @@ public:
 
 	virtual void OnImGuiRender() override
 	{
-
+		ImGui::Begin("Settings");
+		ImGui::ColorEdit3("Square Color", glm::value_ptr(m_SquareColor));
+		ImGui::End();
 	}
 
 	void OnEvent(Amoeba::Event& event) override
@@ -162,7 +186,7 @@ private:
 	std::shared_ptr<Amoeba::Shader> m_Shader;
 	std::shared_ptr<Amoeba::VertexArray> m_VertexArray;
 
-	std::shared_ptr<Amoeba::Shader> m_BlueShader;
+	std::shared_ptr<Amoeba::Shader> m_FlatColorShader;
 	std::shared_ptr<Amoeba::VertexArray> m_SquareVA;
 
 	Amoeba::OrthographicCamera m_Camera;
@@ -171,6 +195,8 @@ private:
 
 	float m_CameraRotation = 0.0f;
 	float m_CameraRotationSpeed = 180.0f;
+
+	glm::vec3 m_SquareColor = { 0.2f, 0.3f, 0.8f };
 };
 
 class Sandbox : public Amoeba::Application
