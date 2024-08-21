da41fc34cf810e70255cb9fe1f91d8706d807103
Alan Magelssen <alanmagelssen@gmail.com>
2024-08-20 19:38:53 -0700

    Add initial texture support

---
+++ Amoeba/src/Amoeba/Renderer/Texture.cpp
@@ -0,0 +1,21 @@
+#include "amoebapch.h"
+#include "Texture.h"
+
+#include "Renderer.h"
+#include "Platform/OpenGL/OpenGLTexture.h"
+
+namespace Amoeba {
+
+	Ref<Texture2D> Texture2D::Create(const std::string& path)
+	{
+		switch (Renderer::GetAPI())
+		{
+			case RendererAPI::API::None:    AMOEBA_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+			case RendererAPI::API::OpenGL:  return std::make_shared<OpenGLTexture2D>(path);
+		}
+
+		AMOEBA_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
+
+}
