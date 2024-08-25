296bb246d7c34778c4b7aed7222b0d9de5a4c3c9
Alan Magelssen <alanmagelssen@gmail.com>
2024-08-24 22:43:55 -0700

    Update Sandbox2D

--- Amoeba/src/Platform/OpenGL/OpenGLBuffer.cpp
+++ Amoeba/src/Platform/OpenGL/OpenGLBuffer.cpp
@@ -1,14 +1,22 @@
 #include "amoebapch.h"
-#include "OpenGLBuffer.h"
+#include "Platform/OpenGL/OpenGLBuffer.h"
 
 #include <glad/glad.h>
 
-namespace Amoeba
-{
+namespace Amoeba {
 
-	/////////////////////////////////////////////////////////////////////////
-	// VertexBuffer /////////////////////////////////////////////////////////
-	/////////////////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////////
+	// VertexBuffer /////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////////
+
+	OpenGLVertexBuffer::OpenGLVertexBuffer(uint32_t size)
+	{
+		AMOEBA_PROFILE_FUNCTION();
+
+		glCreateBuffers(1, &m_RendererID);
+		glBindBuffer(GL_ARRAY_BUFFER, m_RendererID);
+		glBufferData(GL_ARRAY_BUFFER, size, nullptr, GL_DYNAMIC_DRAW);
+	}
 
 	OpenGLVertexBuffer::OpenGLVertexBuffer(float* vertices, uint32_t size)
 	{
@@ -40,9 +48,15 @@ namespace Amoeba
 		glBindBuffer(GL_ARRAY_BUFFER, 0);
 	}
 
-	/////////////////////////////////////////////////////////////////////////
-	// IndexBuffer //////////////////////////////////////////////////////////
-	/////////////////////////////////////////////////////////////////////////
+	void OpenGLVertexBuffer::SetData(const void* data, uint32_t size)
+	{
+		glBindBuffer(GL_ARRAY_BUFFER, m_RendererID);
+		glBufferSubData(GL_ARRAY_BUFFER, 0, size, data);
+	}
+
+	/////////////////////////////////////////////////////////////////////////////
+	// IndexBuffer //////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////////
 
 	OpenGLIndexBuffer::OpenGLIndexBuffer(uint32_t* indices, uint32_t count)
 		: m_Count(count)
@@ -50,8 +64,11 @@ namespace Amoeba
 		AMOEBA_PROFILE_FUNCTION();
 
 		glCreateBuffers(1, &m_RendererID);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_RendererID);
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, count * sizeof(uint32_t), indices, GL_STATIC_DRAW);
+
+		// GL_ELEMENT_ARRAY_BUFFER is not valid without an actively bound VAO
+		// Binding with GL_ARRAY_BUFFER allows the data to be loaded regardless of VAO state. 
+		glBindBuffer(GL_ARRAY_BUFFER, m_RendererID);
+		glBufferData(GL_ARRAY_BUFFER, count * sizeof(uint32_t), indices, GL_STATIC_DRAW);
 	}
 
 	OpenGLIndexBuffer::~OpenGLIndexBuffer()
@@ -60,7 +77,6 @@ namespace Amoeba
 
 		glDeleteBuffers(1, &m_RendererID);
 	}
-
 
 	void OpenGLIndexBuffer::Bind() const
 	{
@@ -75,4 +91,5 @@ namespace Amoeba
 
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 	}
-};
+
+}
